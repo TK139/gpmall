663b1d741f02c92c2220bee74327011a604d2131
TK <134371597+TK139@users.noreply.github.com>
2023-09-23 10:00:29 +0800

    Update jar.sh

--- nginx/jar.sh
+++ nginx/jar.sh
@@ -1,1 +1,10 @@
-
+#!/bin/bash
+nohup java -jar /root/nginx/jar/shopping-provider-0.0.1-SNAPSHOT.jar &
+sleep 5
+nohup java -jar /root/nginx/jar/user-provider-0.0.1-SNAPSHOT.jar &
+sleep 5
+nohup java -jar /root/nginx/jar/gpmall-shopping-0.0.1-SNAPSHOT.jar &
+sleep 5
+nohup java -jar /root/nginx/jar/gpmall-user-0.0.1-SNAPSHOT.jar &
+sleep 5
+ 
